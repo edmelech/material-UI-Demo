3a892044af83b4948c3bc2c47025d4be8d82a8be
Ed Melech <mr.melech@gmail.com>
2022-03-08 19:30:48 +0000

    create seperate styles file for styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import useStyles from './styles';
+
 const App = () => {
+  const classes = useStyles();
+
   return (
    <>
     <CssBaseline />
     <AppBar position='relative'>
       <Toolbar>
-        <PhotoCamera />
+        <PhotoCamera className={classes.icon} />
         <Typography variant="h6">
           Photo Album
         </Typography>
       </Toolbar>
     </AppBar>
     <main>
-      <div>
+      <div className={classes.container}>
         <Container maxWidth="sm">
           <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
             Photo Album
@@ -23,6 +27,20 @@ const App = () => {
           <Typography variant="h5" align="center" color="textSecondary" paragraph>
             Hello everyone. This is a photo album and I'm trying to make this sentence as long as possible so we can see how it looks on the screen.
           </Typography>
+          <div className={classes.button}>
+            <Grid container spacing={2} justify="center">
+              <Grid item>
+                <Button variant="contained" color="primary">
+                  See my photos
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" color="primary">
+                  Secondary action
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
         </Container>
       </div>
     </main>
