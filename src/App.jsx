d8812c5abf3c558453cc016bb1218c43bea2c71a
Ed Melech <mr.melech@gmail.com>
2022-03-08 18:45:14 +0000

    add button group component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 const App = () => {
   return (
    <>
@@ -20,8 +23,16 @@ const App = () => {
           <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
             Photo Album
           </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+            Hello everyone. This is a photo album and I'm trying to make this sentence as long as possible so we can see how it looks on the screen.
+          </Typography>
         </Container>
       </div>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group" color="primary">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
     </main>
    </>
   );
